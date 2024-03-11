8209bd32f63388365bbb4c26b18b818a67e00cb1
Hossein <hfatho2@uic.edu>
2024-03-10 22:38:20 -0500

    GRAVITYP

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-    public GameObject ball; // Assign the Ball GameObject in the inspector
+    public GameObject Cube3; // Assign the Ball GameObject in the inspector
  public float speed=10;
  public float turnspeed=5;
  public float HorizontalInput;
@@ -17,24 +17,24 @@ public class CubeController : MonoBehaviour
     transform.Rotate(Vector3.down*Time.deltaTime*speed*HorizontalInput);
     
     transform.Rotate(Vector3.left*Time.deltaTime*speed*VerticalInput);
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            // Rotate the Cube around its X axis
-            transform.Rotate(90, 0, 0, Space.World);
-            RepositionBall();
-        }
-        else if (Input.GetKeyDown(KeyCode.Y))
-        {
-            // Rotate the Cube around its Y axis
-            transform.Rotate(0, 90, 0, Space.World);
-            RepositionBall();
-        }
+        // if (Input.GetKeyDown(KeyCode.X))
+        // {
+        //     // Rotate the Cube around its X axis
+        //     transform.Rotate(90, 0, 0, Space.World);
+        //     RepositionBall();
+        // }
+        // else if (Input.GetKeyDown(KeyCode.Y))
+        // {
+        //     // Rotate the Cube around its Y axis
+        //     transform.Rotate(0, 90, 0, Space.World);
+        //     RepositionBall();
+        // }
     }
 
     void RepositionBall()
     {
         // Assuming the Cube's pivot is at its center and it has a scale of 1
         // This positions the ball to be on the "top" surface of the Cube after a rotation
-        ball.transform.position = transform.position + Vector3.up * (transform.localScale.y / 2 + ball.transform.localScale.y / 2);
+        //Cube3.transform.position = transform.position + Vector3.up * (transform.localScale.y / 2 + ball.transform.localScale.y / 2);
     }
 }
