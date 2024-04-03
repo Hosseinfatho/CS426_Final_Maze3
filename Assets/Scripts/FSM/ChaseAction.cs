85096f350721c919d45a8bbe6baab2655474a8ac
Dylen <gradyl16@proton.me>
2024-04-02 20:40:14 -0500

    feat: init ai & mecanim feat branch Merge main into branch, organize files, add FSM code & anim code boilerplate

---
+++ Assets/Scripts/FSM/ChaseAction.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(menuName = "FSM/Actions/Chase")]
+public class ChaseAction : FSMAction
+{
+    public override void Execute(BaseStateMachine stateMachine)
+    {
+        var navMeshAgent = stateMachine.GetComponent<NavMeshAgent>();
+        var enemySightSensor = stateMachine.GetComponent<EnemySightSensor>();
+
+        navMeshAgent.SetDestination(enemySightSensor.Player.position);
+    }
+}
