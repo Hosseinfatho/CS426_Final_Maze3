6e8b43b143294672722273dd40183184932fd7d4
Peter Pacholarz <116585257+peterPacho@users.noreply.github.com>
2024-04-06 20:39:30 -0500

    camera rotation still broken

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,5 +1,6 @@
 using System;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -71,15 +72,6 @@ public class Controller : MonoBehaviour
         return Physics.Raycast(gameObject.transform.position, vector, distance, 1 << LayerMask.NameToLayer(layerName));
     }
 
-    /*
-        If ground not detected, then player possibly went over the edge,
-        so we need to rotate the player so we know where the "new" ground is.
-
-        Assumes that at any point it doesn't require to rotate more than 90* in any direction.
-        Rotates only in one axis at the time.
-
-        [If you didn't notice, this function is not very smart]
-    */
     private Vector3[] rotationVectors = { new Vector3(90, 0, 0), new Vector3(-90, 0, 0), new Vector3(0, 90, 0), new Vector3(0, -90, 0), new Vector3(0, 0, 90), new Vector3(0, 0, -90) };
     private void fixPlayerOrientation()
     {
@@ -97,6 +89,30 @@ public class Controller : MonoBehaviour
         }
     }
 
+    private bool cameraInSight()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(mainCamera.transform.position, gameObject.transform.position - mainCamera.transform.position, out hit, 100))
+        {
+            return hit.collider.tag == "Player";
+        }
+
+        return false;
+    }
+
+    private void fixPlayerRotation()
+    {
+        if (!cameraInSight()) return;
+
+        Quaternion lookRot = Quaternion.LookRotation((cameraTopPlane.transform.position - gameObject.transform.position).normalized, getPlayerUpVector());
+        gameObject.transform.rotation = Quaternion.Euler(_roundVector(lookRot.eulerAngles));
+    }
+
+
+
+
+    // Rounds the given number to the closest right angle value (so 44* is rounded to 0, and 46* rounded to 90)
     private float _round(float value)
     {
         float sign = 1;
@@ -113,23 +129,43 @@ public class Controller : MonoBehaviour
         return 90 * mult * sign;
     }
 
-    private void fixPlayerRotation()
+    private Vector3 _roundVector(Vector3 vect)
     {
-        Vector3 rotationVect = Quaternion.LookRotation(cameraTopPlane.transform.position - gameObject.transform.position, gameObject.transform.rotation * Vector3.up).eulerAngles;
-        rotationVect.Set(_round(rotationVect.x), _round(rotationVect.y), _round(rotationVect.z));
-        gameObject.transform.rotation = Quaternion.Euler(rotationVect);
+        vect.Set(_round(vect.x), _round(vect.y), _round(vect.z));
+        return vect;
+    }
+
+    // 
+    private Vector3 _normalizedMaxVector(Vector3 vect)
+    {
+        float max = vect.x;
+
+        if (Math.Abs(vect.y) > Math.Abs(max)) max = vect.y;
+        if (Math.Abs(vect.z) > Math.Abs(max)) max = vect.z;
+
+        if (vect.x != max) vect.x = 0;
+        if (vect.y != max) vect.y = 0;
+        if (vect.z != max) vect.z = 0;
+
+        return vect.normalized;
+    }
+
+    private Vector3 getPlayerUpVector()
+    {
+        return _normalizedMaxVector(gameObject.transform.position);
     }
 
 
     void Update()
     {
-        // Vector that points to the ground
+        // Vector that points to the ground according to the current player's rotation (not actual ground vector)
         Vector3 groundVector = gameObject.transform.rotation * Vector3.down;
 
         // On mouse left click / hold, rotate the camera around the cube
         if (Input.GetMouseButton(0))
         {
             cameraContainer.transform.Rotate(Input.GetAxis("Mouse Y") * -mouseSensitivity, Input.GetAxis("Mouse X") * mouseSensitivity, 0);
+            fixPlayerRotation();
         }
         else
         {
@@ -140,6 +176,7 @@ public class Controller : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             cameraContainer.transform.position = groundVector * -1;
+            cameraContainer.transform.rotation = gameObject.transform.rotation;
             cameraContainer.transform.LookAt(Vector3.zero);
         }
 
@@ -157,7 +194,11 @@ public class Controller : MonoBehaviour
         else
         {
             if (!checkGround(groundVector) && Vector3.Distance(lastGroundPosition, gameObject.transform.position) > 1)
+            {
                 fixPlayerOrientation();
+                fixPlayerRotation();
+            }
+
 
             // this moves the player down towards the face - gravity
             movementVector.y += playerGravity * Time.deltaTime;
@@ -183,6 +224,11 @@ public class Controller : MonoBehaviour
         // Some debug stuff
         debugTextField.text = "";
         debugTextField.text += "Ground vector: " + groundVector.ToString() + "\n";
+        debugTextField.text += "Player vector norm max: " + getPlayerUpVector().ToString() + "\n";
+        debugTextField.text += "Player vector norm max ground: " + (getPlayerUpVector() * -1).ToString() + "\n";
+        debugTextField.text += "Camera max normal: " + (_normalizedMaxVector(cameraContainer.transform.rotation.eulerAngles)).ToString() + "\n";
+        debugTextField.text += "Camera in sight: " + cameraInSight().ToString() + "\n";
+        debugTextField.text += "Main Camera: " + mainCamera.transform.position.ToString() + "\n";
 
         // Use this to rotate the character model towards the velocity vector
         if (movementVector != Vector3.zero)
