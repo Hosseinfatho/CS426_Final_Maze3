6aac91f0bd1fc899a7bc35c5dee628337b40d9e0
gradyl16 <gradyl16@protonmail.com>
2024-04-08 22:36:32 -0500

    feat: basic mecanim for FSM AI Also cleaned up unused assets

--- Assets/Scripts/Enemy/EnemySightSensor.cs
+++ Assets/Scripts/Enemy/EnemySightSensor.cs
@@ -12,9 +12,12 @@ namespace Demo.Enemy
 
         private Ray _ray;
 
+        private Animator anim;
+
         private void Awake()
         {
             Player = GameObject.Find("Player").transform;
+            anim = GetComponentInChildren<Animator>();
         }
 
         public bool Ping()
@@ -22,22 +25,29 @@ namespace Demo.Enemy
             if (Player == null)
                 return false;
 
-            _ray = new Ray(this.transform.position, Player.position-this.transform.position);
+            _ray = new Ray(this.transform.position, Player.position - this.transform.position);
 
             var dir = new Vector3(_ray.direction.x, 0, _ray.direction.z);
 
             var angle = Vector3.Angle(dir, this.transform.forward);
 
-            if (angle>60)
+            if (angle > 60)
                 return false;
 
             if (!Physics.Raycast(_ray, out var hit, 100, ~_ignoreMask))
             {
                 return false;
             }
-            
-            if(hit.collider.tag == "Player")
+
+            if (hit.collider.tag == "Player")
             {
+                // NOTE: probably would be cleaner to move this to EnemyAnimation.cs,
+                //       but don't currently have time to figure out how to obtain reference
+                //       to state from there
+                //
+                // Set trigger for chase animation
+                anim.SetTrigger("Walking2");
+
                 return true;
             }
 
